7bf902f569372c2c4d11d5a8683412e929a47114
KR-ISCOD3 <krisjenxz@example.com>
2025-01-17 15:35:38 +0700

    loading

--- src/page/SignIn.tsx
+++ src/page/SignIn.tsx
@@ -3,6 +3,7 @@ import { NavLink, useNavigate } from "react-router-dom";
 import { useAuth } from "../store/ReactAuthContext";
 import { GoogleLogin } from "@react-oauth/google";
 import axios from "axios";
+import Loading from "./Loading";
 
 function SignIn() {
   const { setUser } = useAuth();
@@ -40,7 +41,7 @@ function SignIn() {
     try {
       // const res = await axios.post("http://localhost:3000/api/auth/login", { token: response.credential });
       const res = await axios.post("https://foot-app-backend.onrender.com/api/auth/login", { token: response.credential });
-
+      setLoading(true);
       if (res.status === 200) {
         setUser(res.data.user);
         navigate("/");
@@ -49,48 +50,58 @@ function SignIn() {
       }
     } catch (error) {
       setError("Error verifying Google login");
+    }finally{
+      setLoading(false);
     }
   };
 
   
   return (
-    <div className="container-fluid d-grid align-content-center font-afacad">
-      <div className="col-12 col-md-5 col-lg-3 mx-auto p-3 pt-5 mt-5">
-        <h1 className="text-center font-bold m-0">Welcome Back!</h1>
-        <p className="text-center mb-3">Let’s get you back to your journey!</p>
-        <form onSubmit={handleSubmit} className="border-b-2 pb-10">
-          <input
-            type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="form-control shadow-none border border-1 my-3"
-            placeholder="Enter Your Username or Email"
-          />
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="form-control shadow-none border border-1 my-3"
-            placeholder="Enter Your Password"
-          />
-          <button type="submit" className="btn text-center w-100 btn-success" disabled={loading}>
-            {loading ? "Signing In..." : "Sign In"}
-          </button>
-          {error && <p className="text-danger mt-2">{error}</p>}
-        </form>
-        <p className="text-center pt-1 border-top mt-3">Or With</p>
-        <div className="d-flex justify-content-center">
-          <GoogleLogin onSuccess={handleSuccess} onError={() => setError("Google login failed")} />
+    <>
+      {
+       loading ? 
+       (<Loading/>)
+       : 
+       (
+       <div className="container-fluid d-grid align-content-center font-afacad">
+        <div className="col-12 col-md-5 col-lg-3 mx-auto p-3 pt-5 mt-5">
+          <h1 className="text-center font-bold m-0">Welcome Back!</h1>
+          <p className="text-center mb-3">Let’s get you back to your journey!</p>
+          <form onSubmit={handleSubmit} className="border-b-2 pb-10">
+            <input
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="form-control shadow-none border border-1 my-3"
+              placeholder="Enter Your Username or Email"
+            />
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="form-control shadow-none border border-1 my-3"
+              placeholder="Enter Your Password"
+            />
+            <button type="submit" className="btn text-center w-100 btn-success" disabled={loading}>
+              {loading ? "Signing In..." : "Sign In"}
+            </button>
+            {error && <p className="text-danger mt-2">{error}</p>}
+          </form>
+          <p className="text-center pt-1 border-top mt-3">Or With</p>
+          <div className="d-flex justify-content-center">
+            <GoogleLogin onSuccess={handleSuccess} onError={() => setError("Google login failed")} />
+          </div>
+          <p className="text-center mt-3">
+            Don’t have an account?
+            <NavLink to={"/signup"} className="text-decoration-none">
+              {" "}
+              Sign Up
+            </NavLink>
+          </p>
         </div>
-        <p className="text-center mt-3">
-          Don’t have an account?
-          <NavLink to={"/signup"} className="text-decoration-none">
-            {" "}
-            Sign Up
-          </NavLink>
-        </p>
-      </div>
-    </div>
+      </div>)
+      }
+    </>
   );
 }
 
